98d54de7597e15bf01d2a0005716e13c53dcffdc
Smirnovious <alex0401@gmail.com>
2022-06-08 22:13:05 +0300

    added more changes

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    const myElement = document.getElementById("about");
+    myElement.style.visibility = "hidden";   
+}
+
+function myFunction2() {
+    const myElement = document.getElementById("about");
+    myElement.style.visibility = "visible";   
+}
