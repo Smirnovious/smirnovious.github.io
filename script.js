bbf7ea53d33f9e7ca970afb4b6af106a2be2ee88
Smirnovious <alex0401@gmail.com>
2022-06-13 16:35:14 +0300

    Some Changes in the responsive Design

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 function myFunction() {
     const myElement = document.getElementById("about");
-    myElement.style.visibility = "hidden";   
+    myElement.style.visibility = "hidden"; 
+    const myElement2 = document.getElementById("clkme");
+    myElement2.style.visibility = "hidden";      
 }
+
 
 function myFunction2() {
     const myElement = document.getElementById("about");
